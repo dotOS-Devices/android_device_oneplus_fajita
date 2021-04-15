aca11468011ec64cff71a9a50396377c45c44a65
SevralT <alex.chycherin56@gmail.com>
2021-04-15 20:56:34 +0300

    fajita: Update to Redfin's April 2021 fingerprint

--- dot_fajita.mk
+++ dot_fajita.mk
@@ -44,4 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus6T \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
